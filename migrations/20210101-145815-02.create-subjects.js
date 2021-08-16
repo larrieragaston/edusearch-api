6b728fbe516855a1af622390b2a5d9ea804ad79f
Gaston Larriera <gaston.larriera@tiarg.com.ar>
2021-08-16 15:57:40 -0300

    Add periodType tu subjects

--- migrations/20210101-145815-02.create-subjects.js
+++ migrations/20210101-145815-02.create-subjects.js
@@ -12,72 +12,84 @@ exports.up = (db, done) => {
       _id: new ObjectId('000000000000000000000000'),
       career: new ObjectId('000000000000000000000000'),
       name: 'Programación I',
+      periodType: 'Yearly',
       curriculum: null
     }, 
     {
       _id: new ObjectId('000000000000000000000001'),
       career: new ObjectId('000000000000000000000000'),
       name: 'Programación II',
+      periodType: 'Yearly',
       curriculum: null
     }, 
     {
       _id: new ObjectId('000000000000000000000002'),
       career: new ObjectId('000000000000000000000000'),
       name: 'Programación III',
+      periodType: 'Yearly',
       curriculum: null
     }, 
     {
       _id: new ObjectId('000000000000000000000003'),
       career: new ObjectId('000000000000000000000001'),
       name: 'Electrónica I',
+      periodType: 'Yearly',
       curriculum: null
     }, 
     {
       _id: new ObjectId('000000000000000000000004'),
       career: new ObjectId('000000000000000000000001'),
       name: 'Electrónica II',
+      periodType: 'Yearly',
       curriculum: null
     }, 
     {
       _id: new ObjectId('000000000000000000000005'),
       career: new ObjectId('000000000000000000000001'),
       name: 'Electrónica III',
+      periodType: 'Yearly',
       curriculum: null
     }, 
     {
       _id: new ObjectId('000000000000000000000006'),
       career: new ObjectId('000000000000000000000002'),
       name: 'Programación I',
+      periodType: 'Quarterly',
       curriculum: null
     }, 
     {
       _id: new ObjectId('000000000000000000000007'),
       career: new ObjectId('000000000000000000000002'),
       name: 'Programación II',
+      periodType: 'Quarterly',
       curriculum: null
     }, 
     {
       _id: new ObjectId('000000000000000000000008'),
       career: new ObjectId('000000000000000000000002'),
       name: 'Programación III',
+      periodType: 'Quarterly',
       curriculum: null
     }, 
     {
       _id: new ObjectId('000000000000000000000009'),
       career: new ObjectId('000000000000000000000003'),
       name: 'Electrónica I',
+      periodType: 'Quarterly',
       curriculum: null
     }, 
     {
       _id: new ObjectId('000000000000000000000010'),
       career: new ObjectId('000000000000000000000003'),
       name: 'Electrónica II',
+      periodType: 'Quarterly',
       curriculum: null
     }, 
     {
       _id: new ObjectId('000000000000000000000011'),
       career: new ObjectId('000000000000000000000003'),
       name: 'Electrónica III',
+      periodType: 'Quarterly',
       curriculum: null
     }
   ],
