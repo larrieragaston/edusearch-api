98700367832490b6d895cb0558c824a0e3fdefd7
Gaston Larriera <gaston.larriera@tiarg.com.ar>
2021-08-16 23:20:53 -0300

    favourites and postulations routes

--- components/contest/route.js
+++ components/contest/route.js
@@ -4,10 +4,45 @@ const authenticate = require('../../src/authentication')
 const router = new Router()
 
 router.get('/contests/getContestById/:id', authenticate, findContestById)
-router.get('/contests/contestForUser', authenticate, findContestForUser)
+router.get('/contests/contestsForUser', authenticate, findContestsForUser)
+router.get('/contests/postulationsForUser', authenticate, findPostulationsForUser)
+router.get('/contests/favouriteForUser', authenticate, findFavouritesForUser)
 
-async function findContestForUser(req, res, next) {
-  req.logger.info(`Finding contests for user with id ${req.user._id} using auth token`)
+async function findContestById(req, res, next) {
+  req.logger.info(`Finding contest with id ${req.params.id} using auth token`)
+  
+  try {
+    let contest = await req
+    .model('Contest')
+      .findOne({ _id: req.params.id })
+      .populate('university')
+      .populate('career')
+      .populate('subject')
+
+      if (!contest) {
+        req.logger.verbose('Contest not found. Sending 404 to client')
+      return res.status(404).end()
+    }
+    
+    const postulation = await req
+    .model('Postulation')
+    .find({ contest: req.params.id, user: req.user._id })
+    
+    const favourite = await req
+    .model('Favourite')
+    .find({ contest: req.params.id, user: req.user._id })
+    
+    const contestWithPostulation = { ...contest._doc, hasPostulation: postulation.length > 0, isFavourite: favourite.length > 0 }
+    
+    req.logger.verbose('Sending contestWithPostulation to client')
+    return res.json(contestWithPostulation)
+  } catch (err) {
+    return next(err)
+  }
+}
+
+async function findContestsForUser(req, res, next) {
+  req.logger.info(`Finding all contests for user with id ${req.user._id} using auth token`)
 
   try {
     const contests = await req
@@ -31,43 +66,78 @@ async function findContestForUser(req, res, next) {
       return { ...contest._doc, hasPostulation, isFavourite }
     })
 
-    req.logger.verbose('Sending contestsWithPostulations to client')
+    req.logger.verbose('Sending all contestsWithPostulations to client')
     res.json(contestsWithPostulations)
   } catch (err) {
     next(err)
   }
 }
 
-async function findContestById(req, res, next) {
-  req.logger.info(`Finding contest with id ${req.params.id} using auth token`)
+async function findPostulationsForUser(req, res, next) {
+  req.logger.info(`Finding postulation contests for user with id ${req.user._id} using auth token`)
 
   try {
-    let contest = await req
+    const contests = await req
       .model('Contest')
-      .findOne({ _id: req.params.id })
+      .find({})
       .populate('university')
       .populate('career')
       .populate('subject')
 
-    if (!contest) {
-      req.logger.verbose('Contest not found. Sending 404 to client')
-      return res.status(404).end()
-    }
-
-    const postulation = await req
+    const postulations = await req
       .model('Postulation')
-      .find({ contest: req.params.id, user: req.user._id })
-
-    const favourite = await req
+      .find({ user: req.user._id })
+      
+    const favourites = await req
       .model('Favourite')
-      .find({ contest: req.params.id, user: req.user._id })
+      .find({ user: req.user._id })
 
-    const contestWithPostulation = { ...contest._doc, hasPostulation: postulation.length > 0, isFavourite: favourite.length > 0 }
+    const contestsWithPostulations = contests
+      .filter(x => postulations.some(p => p.contest.equals(x._id)))
+      .map(function (contest) {
+        const hasPostulation = postulations.some(p => p.contest.equals(contest._id))
+        const isFavourite = favourites.some(p => p.contest.equals(contest._id))
+        return { ...contest._doc, hasPostulation, isFavourite }
+      })
 
-    req.logger.verbose('Sending contestWithPostulation to client')
-    return res.json(contestWithPostulation)
+    req.logger.verbose('Sending postulations contestsWithPostulations to client')
+    res.json(contestsWithPostulations)
   } catch (err) {
-    return next(err)
+    next(err)
+  }
+}
+
+async function findFavouritesForUser(req, res, next) {
+  req.logger.info(`Finding favourite contests for user with id ${req.user._id} using auth token`)
+
+  try {
+    const contests = await req
+      .model('Contest')
+      .find({})
+      .populate('university')
+      .populate('career')
+      .populate('subject')
+
+    const postulations = await req
+      .model('Postulation')
+      .find({ user: req.user._id })
+      
+    const favourites = await req
+      .model('Favourite')
+      .find({ user: req.user._id })
+
+    const contestsWithPostulations = contests
+      .filter(x => favourites.some(p => p.contest.equals(x._id)))
+      .map(function (contest) {
+        const hasPostulation = postulations.some(p => p.contest.equals(contest._id))
+        const isFavourite = favourites.some(p => p.contest.equals(contest._id))
+        return { ...contest._doc, hasPostulation, isFavourite }
+      })
+
+    req.logger.verbose('Sending favourite contestsWithPostulations to client')
+    res.json(contestsWithPostulations)
+  } catch (err) {
+    next(err)
   }
 }
 
